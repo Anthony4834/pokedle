ce240ac005fb1eb14f0f3991de3562dd86cf0bc1
anthony.griffin <anthony.griffin@workday.com>
2024-05-25 18:56:08 -0700

    config

--- server/server.mjs
+++ server/server.mjs
@@ -6,8 +6,8 @@ import pokemon from './routes/pokemon.mjs';
 import success from './routes/success.mjs';
 import tiktok from './routes/tiktok.mjs';
 
-const PORT = process.env.PORT || 5050;
-export const BASE_QUERY = 'https://pokedle-lyart.vercel.app';
+// Assuming that you have your environment variables loaded here
+const BASE_QUERY = 'https://pokedle-lyart.vercel.app';
 
 const app = express();
 
@@ -17,19 +17,7 @@ app.use(express.json());
 app.use("/players", players);
 app.use("/success", success);
 app.use('/pokemon', pokemon);
-app.use('/tiktok', tiktok)
+app.use('/tiktok', tiktok);
 
-// start the Express server
-app.listen(PORT, () => {
-  console.log(`Server is running on port: ${PORT}`);
-});
-
-/* Commenting out because cyclic allows cron expressions to be defined via their dashboard */
-// cron.schedule("0 0 * * *", function() {
-//     console.log("running a task every minute");
-//     request.post(BASE_QUERY + '/pokemon', function (error, response, body) {
-//        if (!error && response.statusCode == 200) {
-//           console.log(body) // Print the google web page.
-//        }
-//     })
-// })
+// Export the app as a function to be used by Vercel
+export default app;
